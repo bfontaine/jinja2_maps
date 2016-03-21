2c762d875c7c2f9bd89f9c1237e3b541abc76868
Baptiste Fontaine <batifon@yahoo.fr>
2016-03-22 00:40:44 +0100

    PEP8 compliance

--- jinja2_maps/gmaps.py
+++ jinja2_maps/gmaps.py
@@ -3,6 +3,7 @@
 from .base import _make_url_filter
 
 # http://stackoverflow.com/a/25993441/735926
-gmaps_url = _make_url_filter("gmaps_url", "Google Maps",
+gmaps_url = _make_url_filter(
+        "gmaps_url", "Google Maps",
         "https://www.google.com/maps/place/{lat},{lng}/@{lat},{lng},{zoom}z",
         zoom=16)
