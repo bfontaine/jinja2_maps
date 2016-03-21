308ae56a017ef61e4d9bb1733e104e63e9dd04ed
Baptiste Fontaine <batifon@yahoo.fr>
2016-03-21 11:48:08 +0100

    Main helpers tests

--- tests/test.py
+++ tests/test.py
@@ -7,11 +7,21 @@ if __name__ == "__main__":
     here = dirname(__file__)
     sys.path.insert(0, here+"/..")
 
+from jinja2 import Environment
 import jinja2_maps
 
-class TestJinja2MapsVersion(TestCase):
+class TestJinja2Maps(TestCase):
     def test_version(self):
         self.assertRegexpMatches(jinja2_maps.__version__, r"^\d+\.\d+\.\d+")
+
+    def test_filters(self):
+        self.assertEquals(["gmaps_url", "osm_map", "osm_url"],
+                sorted(jinja2_maps.filters().keys()))
+
+    def test_activate_filters(self):
+        env = Environment()
+        jinja2_maps.activate_filters(env)
+        self.assertIn("gmaps_url", env.filters)
 
 if __name__ == "__main__":
     suite = unittest.defaultTestLoader.discover(here)
